d45a19c93c8e6e4dec5f0e1482c19cb645cbcf28
Maksim V <maks112v@gmail.com>
2019-07-23 20:23:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">Welcome to [Project Name]👋</h1>
+<p>
+  <img src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/giturl.git">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" target="_blank" />
+  </a>
+</p>
+
+> The fastest way to start with firebase
+
+### 🏠 [Homepage](Project Url)
+
+## Install
+
+```sh
+npm install
+```
+
+## Author
+
+👤 ** Your Name **
+
+- Github: [Git Name](GitUrl)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ src/config/firebase.js
@@ -0,0 +1,36 @@
+import * as firebase from 'firebase';
+
+// Add the Firebase products that you want to use
+// import 'firebase/auth';
+// import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// firebase
+//   .firestore()
+//   .enablePersistence()
+//   .catch(function(err) {
+//     if (err.code == 'failed-precondition') {
+//       // Multiple tabs open, persistence can only be enabled
+//       // in one tab at a a time.
+//       // ...
+//     } else if (err.code == 'unimplemented') {
+//       // The current browser does not support all of the
+//       // features required to enable persistence
+//       // ...
+//     }
+//   });
+
+export const store = firebase.firestore();
+
+export default firebase;
